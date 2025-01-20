1ef9e8a946a2e2f20c91b63b00f75706cb4f3d5f
mrinal1224 <iammrinalkc@gmail.com>
2025-01-20 09:58:57 +0530

    push

---
+++ Class-9 (Async-2 Promises)/cb.js
@@ -0,0 +1,23 @@
+function prepareIngredients(){
+  setTimeout(function(){
+    console.log('Ingredients Prepared')
+  })
+}
+
+function cookDish(){
+    setTimeout(function(){
+      console.log('Dish cooked')
+    })
+  }
+
+  function serveDish(){
+    setTimeout(function(){
+      console.log('Dish Served')
+    })
+  }
+
+  function cleanUp(){
+    setTimeout(function(){
+      console.log("Table cleaned")
+    })
+  }
