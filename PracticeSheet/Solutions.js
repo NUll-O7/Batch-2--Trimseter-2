2ae0b9937cf383b568007dd68a6218267b1b3e44
NUll_O7 <dhruvsonimain@gmail.com>
2025-01-21 11:02:21 +0530

    MyChanges 2

---
+++ PracticeSheet/Solutions.js
@@ -0,0 +1,9 @@
+//1. What is asynchronous programming, and why is it important in JavaScript?
+/*sol1) Asyunchronous programming is a programming paradigm that allows for the execution of multiple tasks concurrently, improving the responsiveness and efficiency of the program
+while allowing the webpage to be responsive.*/
+
+//2. 2. Explain the difference between synchronous and asynchronous code execution.
+/*sol2) Synchronous code execution executes one line of code at a time, waiting for the task on hand to complete on the other hand
+asynchronous code execution executes multiple lines of code at the same time, allowing the webpage to be responsive while the code is executing.*/
+
+//3. What is a callback function? Provide an example.
